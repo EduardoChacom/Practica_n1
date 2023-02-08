ce0bae455d7be03f2155219ea35743e7c56187b0
eduardoChacom <lchacon0@ucol.mx>
2023-02-08 08:07:12 -0600

    Actualización 1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { toCelsius } = require("celsius");
+const { toCelsius } = require("celsius"); //Inyectar todo lo que vamos a usar.
  
 console.log(toCelsius(100)); // 38
 console.log(toCelsius(100, 3)); // 37.778
@@ -10,7 +10,11 @@ console.log(toFahrenheit(36)); // 97
 console.log(toFahrenheit(36.68, 3)); // 98.024
 console.log(toFahrenheit('-40.691 degrees C', 5)); // -41.24380
 
-const convertidor = require ("celsius");
+const convertidor = require ("celsius"); //trae todo lo que tiene el modulo.
 
-console.log(convertidor.toCelsius(20));
-console.log(convertidor.toFahrenheit(30));
+console.log(convertidor.toCelsius(20)); //el convertidor trae todo 
+console.log(convertidor.toFahrenheit(30)); //puedes usar todo lo que tiene.
+
+//npm init -y para instalar
+//npm install celsius - ejemplo, para instalar
+//npm i celcius
