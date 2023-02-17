1d08b292d1cb948cbad3f25ddb1efefd533262b8
EduardoChacom <90471214+EduardoChacom@users.noreply.github.com>
2023-02-17 14:54:43 -0600

    Delete modules directory

--- modules/app.js
+++
@@ -1,5 +0,0 @@
-let greet = require('./greet'); //Inyectamos todo lo de nuestra dependencia.
-
-greet(); /*Referencía de memoria Alojado! es un error escribirlo así
-           El codigo dentro de cada modulo es "self contained" 
-           y su ámbito de acceso es "protected" ocupas "module.exports*/
--- modules/greet.js
+++
@@ -1,4 +0,0 @@
-let greet = () => console.log('Hello from the module');
-
-module.exports = greet;
-//con "module.exports" la función ya puede salir
