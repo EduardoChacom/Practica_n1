8dc9ce73dfddcf70504f13a925553d16c97c2d89
EduardoChacom <90471214+EduardoChacom@users.noreply.github.com>
2023-02-24 08:25:10 -0600

    Delete patrones directory

--- patrones/greet1.js
+++
@@ -1,6 +0,0 @@
-//Moodulo que exporta solo una función.
-let greet = () => {
-    console.log('Hello from greet1!');
-}
-
-module.exports = greet;
--- patrones/greet4.js
+++
@@ -1,6 +0,0 @@
-module.exports = function Greetr() {
-    this.greeting = "Hello from the constructor function greet 4";
-    this.greet = function () {
-        console.log(this.greeting);
-    }
-}
