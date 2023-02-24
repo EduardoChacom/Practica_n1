a7a44da2bc93246c93148394ea26f9e8abfed249
EduardoChacom <90471214+EduardoChacom@users.noreply.github.com>
2023-02-24 07:59:57 -0600

    Delete patrones directory

--- patrones/greet3.js
+++
@@ -1,8 +0,0 @@
-function Greetr() {
-    this.greeting = "Hello from the constructor function";
-    this.greet = function () {
-        console.log(this.greeting);
-    }
-}
-
-module.exports = new Greetr;
